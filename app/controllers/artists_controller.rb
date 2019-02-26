255b566be26c7e7ccbcbd82bf6edab8eaf824e44
Angelo Spampinato <spampinatodesign@gmail.com>
2019-02-26 14:46:07 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,13 @@
 class ArtistsController < ApplicationController
+  
+  before_action :find_artist, only: [:show, :edit, :update]
+
   def index
+    @artists = Artist.all
   end
 
   def show
+
   end
 
   def new
@@ -11,7 +16,6 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.new(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -20,14 +24,10 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -43,6 +43,10 @@ class ArtistsController < ApplicationController
   end
 
   private
+
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
     params.require(:artist).permit(:name)
