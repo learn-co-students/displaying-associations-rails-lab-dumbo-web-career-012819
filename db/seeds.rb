dfd9914cc96a43854d29c09bbbef6134d7705d73
mazenswar <mazenswar@protonmail.com>
2019-02-26 15:09:29 -0500

    displaying associations lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+queen = Artist.create(name: "Queen")
+Song.create(title: "I'm Going Slightly Mad", artist: queen)
+Song.create(title: "Don't Stop Me Now", artist: queen)
+Song.create(title: "Bohemian Rhapsody", artist: queen)
+Song.create(title: "Papaoutai", artist: Artist.create(name: "Stromae"))
+Song.create(title: "In the End", artist: Artist.create(name: "Linkin Park"))
+Song.create(title: "7abaytak", artist: Artist.create(name: "Fairouz"))
